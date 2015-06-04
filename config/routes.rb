fe057ae07a8156ccdf8bd0fbf041fcfac44dbdd0
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-06-04 16:32:32 -0400

    fixed nested routes for project votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :profiles
-  resources :projects
+  resources :projects do
+    member do
+      put "like",  to: "projects#upvote"
+      puts "dislike", to: "projects#downvote"
+    end
+  end
 
   root to: 'pages#index'
 
