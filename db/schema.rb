39b84ed5211c71ad6131d2d325196f026a6f6c27
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-06-09 13:28:46 -0400

    Add Linkedin to Profile migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609172006) do
+ActiveRecord::Schema.define(version: 20150609172808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150609172006) do
     t.text     "bio"
     t.string   "facebook"
     t.string   "twitter"
+    t.string   "linkedin"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
