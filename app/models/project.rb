c8687168a1c4b5c713446cf4dfe8f00f1a182ffd
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-06-13 18:25:48 -0400

    add required fields to forms

--- app/models/project.rb
+++ app/models/project.rb
@@ -25,30 +25,10 @@ class Project < ActiveRecord::Base
     {:bucket => "dev-node", :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]}
   end
 
-  # after_save do
-  #   video_hash = JSON.parse(open("https://vimeo.com/api/oembed.json?url=#{self.video}").read)
-  #   self.video_thumb = video_hash['thumbnail_url']
-  #   self.save
-  # end
-
   def video_embed
     video.gsub("vimeo.com/","player.vimeo.com/video/")
   end
-  #https://vimeo.com/128028122
-  # #Set video url to include http:// if not included
-  # def set_video_url
-  #   url = @project.video
-  #   @project.video = full_url
-  # end
-  
-  # #Check for full url
-  # def full_url
-  #   if url.match("http://") || url.match("http://")
-  #     url = "http://"+url
-  #   else
-  #     url
-  #  end
-  #end
+
 end
 
 Project.import # for auto sync model with elastic search
