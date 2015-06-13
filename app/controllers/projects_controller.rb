f5f7e9f82c74cf3381ab6a8a9fa8f771e1ec693f
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-06-12 22:41:02 -0400

    Add picture to project forms

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -103,6 +103,6 @@ class ProjectsController < ApplicationController
 		end
 
 		def project_params
-			params.require(:project).permit(:name, :description, :video, :repo, :tag_list, :fulldescription, :languages, :q)
+			params.require(:project).permit(:name, :description, :video, :repo, :tag_list, :fulldescription, :languages, :q, :picture)
 		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610232227) do
+ActiveRecord::Schema.define(version: 20150612233330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,10 @@ ActiveRecord::Schema.define(version: 20150610232227) do
     t.integer  "cached_weighted_score",   default: 0
     t.integer  "cached_weighted_total",   default: 0
     t.float    "cached_weighted_average", default: 0.0
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   add_index "projects", ["cached_votes_down"], name: "index_projects_on_cached_votes_down", using: :btree
